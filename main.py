f3d1d3e7baf753cdc9cb55f4438a32f8bde2492f
Tomasz Roszczynialski <roszcz@gmail.com>
2021-08-02 10:17:35 +0200

    fit the second pendulum into the frame

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def scene_from_trajectory(df):
     M.config['video_dir'] = 'media/'
     M.config['images_dir'] = 'media/'
     # M.config['save_last_frame'] = True
-    
+
     dt = 0.01
     l1 = 1.5
     l2 = 0.5
 
     # NumberPlane units (2 * "radius")
-    M.config['frame_width'] = 2 * (l1+l2)
-    M.config['frame_height'] = 2 * (l1+l2)
+    M.config['frame_width'] = 2 * (l1+l2) * 1.1
+    M.config['frame_height'] = 2 * (l1+l2) * 1.1
 
     output_name = 'cycle-search-0x08-small'
     M.config['output_file'] = output_name
